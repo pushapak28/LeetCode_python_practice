0b2693de65e1222ed22551f404ca8256b1604a76
Pushapak Kumar <pp281997@gmail.com>
2022-05-10 14:15:02 +0530

    Update and rename Average Salary Excluding the Minimum and Maximum Salary to average_list_element.py

--- average_list_element.py
+++ average_list_element.py
@@ -1,3 +1,4 @@
+#Average Salary Excluding the Minimum and Maximum Salary
 #You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
 
 #Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
